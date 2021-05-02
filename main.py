07ad68d933dc167725dcbfd4c6ee8514be6fd42f
Brandon Allen <>
2021-05-02 18:40:53 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,66 @@
+<!DOCTYPEhtml>
+<!-- This a comment -->
+<html lang="en-ca">
+  <head> 
+  <h1 style="background-color:DodgerBlue;">The Upcoming Of Bill Gates</h1>
+  <title>="The Upcoming Of Bill Gates"<title> 
+  <p style="font size:130%">The Upcoming Of Bill Gates</p>
+  <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F988775660163252226%2FXpgonN0X_400x400.jpg&imgrefurl=https%3A%2F%2Ftwitter.com%2Fbillgates&tbnid=UXFPGSqlXS5o4M&vet=12ahUKEwjn_ofHgqHwAhW0ds0KHaAdDvAQMygDegUIARDXAQ..i&docid=Z2D5c1IzoQZsJM&w=399&h=399&q=Bill%20Gates&safe=active&ved=2ahUKEwjn_ofHgqHwAhW0ds0KHaAdDvAQMygDegUIARDXAQ"width="500" height="600"alt="Local Image">
+  <title>= "Bill Gates Contribution To The World Of Computer Science"
+  <p style= "font size:90%"> Bill Gates Contribution To The World Of Computer Science</p>
+  <p> Entrepreneur and businessman Bill Gates and his business partner Paul Allen founded and built the world's largest software business, Microsoft, through technological innovation, keen business strategy and aggressive business tactics. In the process, Gates became one of the richest men in the world.</p>
+<title>= "The History Of Bill Gates"
+<p style= "font size:90%"> The History Of Bill Gates</p>
+<p> Bill Gates, in full William Henry Gates III, (born October 28, 1955, Seattle, Washington, U.S.), American computer programmer and entrepreneur who cofounded Microsoft Corporation, the world's largest personal-computer software company.</p>
+<title>="Quote From Himself Bill Gates"<title>
+<p style= "font size:90%">Quote From The Man Himself Bill Gates</p>
+<p>"Success is a lousy teacher. It seduces smart people into thinking they can't lose".<p>
+<i>Success is a lousy teacher. It seduces smart people into thinking they can't lose.</i>
+
+<head>
+      <meta name="viewport" content="width-device-width, intial=scale=1, user- scalable=no"
+      <title>Pure CSS STICKY Navbar</title>
+      <link rel="stylesheet" type="text/css" href="style.css"
+  <head>
+  <body>
+      <header><img src="logo.jpg"></header>
+      <section class="sec1"></section>
+      <section class="sec2">
+          <h2>Pure CSS STICKY NAVBAR - Javascpirt</h2>
+          <p></p>
+      </section>
+      <section class="sec3"></section>
+  </body>
+</html>
+<head>
+</head>
+<body>
+      <style>
+      *{
+          box-sizing: border-box
+      }
+      .child{
+          border : Spx solid blue;
+      }
+      @media (min-width: 768px){}
+      .parent{
+          display:flex;
+      }
+      }
+      }
+<section>
+    <section class="child">
+      "Entrepreneur and businessman Bill Gates and his business partner Paul Allen founded and built the world's largest software business, Microsoft, through technological innovation, keen business strategy and aggressive business tactics. In the process, Gates became one of the richest men in the world."
+
+    </section>
+    <section class="child">
+      "Bill Gates, in full William Henry Gates III, (born October 28, 1955, Seattle, Washington, U.S.), American computer programmer and entrepreneur who cofounded Microsoft Corporation, the world's largest personal-computer software company."
+         </section>
+
+         </section>
+    <section class="child">
+      "Success is a lousy teacher. It seduces smart people into thinking they can't lose"
+      </section>
+         https://www.google.com/imgres?imgurl=https%3A%2F%2Fpbs.twimg.com%2Fprofile_images%2F988775660163252226%2FXpgonN0X_400x400.jpg&imgrefurl=https%3A%2F%2Ftwitter.com%2Fbillgates&tbnid=UXFPGSqlXS5o4M&vet=12ahUKEwjn_ofHgqHwAhW0ds0KHaAdDvAQMygDegUIARDXAQ..i&docid=Z2D5c1IzoQZsJM&w=399&h=399&q=Bill%20Gates&safe=active&ved=2ahUKEwjn_ofHgqHwAhW0ds0KHaAdDvAQMygDegUIARDXAQ"width="500" height="600"alt="Local Image">
+      </body>
+      </html>
